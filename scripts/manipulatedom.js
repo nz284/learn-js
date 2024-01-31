6617266d89048d5669588546485230cbdba827a7
nz284 <nanxiangzhao@gmail.com>
2024-01-30 20:50:53 -0500

    feb01

--- scripts/manipulatedom.js
+++ scripts/manipulatedom.js
@@ -10,14 +10,16 @@ function createTRNode(colNodes) {
   return trNode;
 }
 
-function createTDNode(childNode) {
-  let tdNode = document.createElement("td");
-  tdNode.appendChild(childNode);
-  return tdNode;
+function createTDNode(childNode,e,l) {
+  let btnNode = document.createElement("button");
+  let btnNodeText=createTxtNode(btnTxt);
+  btnNode.appendChild(btnNodeText);
+  btnNode.addEventListener(e,l);
+  return btnNode;
 }
 
-function createTxtNode(txt) {
-  let txtNode = document.createTextNode(txt);
+function createTxtNode(childNode) {
+  let childNode = document.createTextNode(txt);
   return txtNode;
 }
 
@@ -25,7 +27,17 @@ function addTable() {
   const tableNode = document.createElement("table");
   for(let i = 0; i < 3; i++) {
     let col1 = createTDNode(createTxtNode("Cell (" + i + ", 0)"));
+    let col2= createTDNode(createTxtNode("Edit text","click",function(){
+      edit(col1);
+    })
     tableNode.appendChild(createTRNode([col1]));
   }
   document.getElementById("root").appendChild(tableNode);
+}
+
+function edit(node){
+  let newNode=document.createElement("input");
+  newNode.type='text';
+  newNode.placeholder='Enter Cell (x,y) ...';
+  node.replaceChild(newNode,node.childNodes[0]);
 }
